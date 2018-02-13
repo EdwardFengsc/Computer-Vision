6e21bceb1077430a234ebd0168a728aaf6789278
EdwardFengsc <31291202+EdwardFengsc@users.noreply.github.com>
2018-02-12 23:37:30 -0500

    Update hybrid.py

--- hybrid.py
+++ hybrid.py
@@ -5,6 +5,30 @@ import cv2
 import numpy as np
 
 def cross_correlation_2d(img, kernel):
+	k_row=(kernel.shape(0)-1)/2
+	k_col=(kernel.shape(1)-1)/2
+	
+	kernel.shape=(1,len(kernel.flatten()))
+	kernel=np.transpose(kernel)
+	img_d=img.ndim
+	correlation_img=np.zeros((img.shape[0],img.shape[1]),dtype=float)
+	
+	if img_d==2:
+    	img=img.reshape((img.shape[0],img.shape[1],1))
+    
+    for d in range(img.shape[2]):
+    	img_gray=img[:,:,d]
+
+    	for i in range(k_row,img_gray.shape[0]-k_row):
+        	for j in range(k_col,img_gray.shape[1]-k_col):
+	            g=[]
+	            img_gray_s=img_gray[i-k_row:i+k_row+1,j-k_col:j+k_col+1]
+	            img_gray_s=img_grays.flatten()
+	            
+	            g=np.matrix.dot(img_gray_s,kernel)
+	            correlation_img[i,j,d]=np.sum(g)
+
+	 return correlation_img
     '''Given a kernel of arbitrary m x n dimensions, with both m and n being
     odd, compute the cross correlation of the given image with the given
     kernel, such that the output is of the same dimensions as the image and that
@@ -27,6 +51,9 @@ def cross_correlation_2d(img, kernel):
     # TODO-BLOCK-END
 
 def convolve_2d(img, kernel):
+	kernel_trans=kernel.transpose()
+
+	retun cross_correlation_2d(img,kernel_trans)
     '''Use cross_correlation_2d() to carry out a 2D convolution.
 
     Inputs:
@@ -44,6 +71,16 @@ def convolve_2d(img, kernel):
     # TODO-BLOCK-END
 
 def gaussian_blur_kernel_2d(sigma, width, height):
+	w_h=int((width-1)/2)
+	h_h=int((height-1)/2)
+	pi=np.pi
+	
+	a=np.array([[x**2+y**2 for x in range(-w_h,w_h+1)] for y in range(-h_h, h_h+1)])
+	
+	GaussianMatrix=1/(2*pi*sigma**2)*np.exp(-a/(2*sigma**2))
+
+	return GaussianMatrix
+
     '''Return a Gaussian blur kernel of the given dimensions and with the given
     sigma. Note that width and height are different.
 
@@ -63,6 +100,10 @@ def gaussian_blur_kernel_2d(sigma, width, height):
     # TODO-BLOCK-END
 
 def low_pass(img, sigma, size):
+	Gaussian_kernel=gaussian_blur_kernel_2d(sigma,size,size)
+	low_pass_img=convolve_2d(img,Gaussian_kernel)
+	return low_pass_img
+
     '''Filter the image as if its filtered with a low pass filter of the given
     sigma and a square kernel of the given size. A low pass filter supresses
     the higher frequency components (finer details) of the image.
@@ -76,6 +117,9 @@ def low_pass(img, sigma, size):
     # TODO-BLOCK-END
 
 def high_pass(img, sigma, size):
+	low_pass_img=low_pass(img, sigma, size)
+	high_pass_img=img-low_pass_img
+	return high_pass_img
     '''Filter the image as if its filtered with a high pass filter of the given
     sigma and a square kernel of the given size. A high pass filter suppresses
     the lower frequency components (coarse details) of the image.
