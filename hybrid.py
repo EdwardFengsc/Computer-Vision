9f87b9981a75071ad472a9da5a21c65f6d06b037
EdwardFengsc <31291202+EdwardFengsc@users.noreply.github.com>
2018-02-13 00:01:29 -0500

    Update hybrid.py

--- hybrid.py
+++ hybrid.py
@@ -43,9 +43,10 @@ def cross_correlation_2d(img, kernel):
     cross_corr_img_save=np.empty(img.shape)
 #     cross_corr_img_save=np.zeros(img.shape)
     #doing cross_correlation operation
+    kernelr=kernel.reshape(-1)
     for i in range(i_height):
         for j in range(i_width):
-            cross_corr_img_save[i,j]=np.dot(kernel.reshape(-1),np.reshape(cross_corr_img_operation[i:i+k_height,j:j+k_width],
+            cross_corr_img_save[i,j]=np.dot(kernelr,np.reshape(cross_corr_img_operation[i:i+k_height,j:j+k_width],
                                                        (k_height*k_width,i_rgb)))
     return cross_corr_img_save
 
