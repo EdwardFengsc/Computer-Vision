8c302f0a9dee4e321d47169654a4c828a89f774c
EdwardFengsc <31291202+EdwardFengsc@users.noreply.github.com>
2018-02-13 00:37:30 -0500

    Update hybrid.py

--- hybrid.py
+++ hybrid.py
@@ -25,31 +25,50 @@ def cross_correlation_2d(img, kernel):
         height and the number of color channels)
     '''
     k_height, k_width=kernel.shape
-    cross_corr_save=np.zeros(img.shape)
-    # keep the image into 3 dimensions
+    cross_corr_img_save=np.zeros(img.shape)
     if len(img.shape)==2:
-        i_height,i_width = img.shape
+        i_height,i_width=img.shape
         i_rgb=1
         img=np.expand_dims(img,axis=2)
     else:
         i_height,i_width,i_rgb=img.shape
-    #set up a new workplace adding size of kernels and images
-    cross_corr_operation = np.zeros(( k_height + i_height - 1, k_width + i_width - 1,i_rgb), dtype=img.dtype)
+    #operation variant
+    cross_corr_img_operation=np.zeros((i_height+k_height-1,i_width+k_width-1,i_rgb),dtype=img.dtype)
+    cross_corr_img_operation[(k_height-1)/2:(k_height-1)/2+i_height,(k_width-1)/2:(k_width-1)/2+i_width]=img
+    #return variant
+
+    #doing cross_correlation operation
+    for i in range(i_height):
+        for j in range(i_width):
+            cross_corr_img_save[i,j]=np.dot(np.reshape(cross_corr_img_operation[i:i+k_height,j:j+k_width],
+                                                       (k_height*k_width,i_rgb)),kernel.reshape(-1))
+    return cross_corr_img_save
+#     k_height, k_width=kernel.shape
+#     cross_corr_save=np.zeros(img.shape)
+#     # keep the image into 3 dimensions
+#     if len(img.shape)==2:
+#         i_height,i_width = img.shape
+#         i_rgb=1
+#         img=np.expand_dims(img,axis=2)
+#     else:
+#         i_height,i_width,i_rgb=img.shape
+#     #set up a new workplace adding size of kernels and images
+#     cross_corr_operation = np.zeros(( k_height + i_height - 1, k_width + i_width - 1,i_rgb), dtype=img.dtype)
     
-    k_height1 = (k_height - 1) / 2
-    k_width1 = (k_width - 1) / 2
-    # put the image into the workplace
-    cross_corr_operation[k_height1:k_height1+i_height, k_width1:k_width1+i_width] = img
+#     k_height1 = (k_height - 1) / 2
+#     k_width1 = (k_width - 1) / 2
+#     # put the image into the workplace
+#     cross_corr_operation[k_height1:k_height1+i_height, k_width1:k_width1+i_width] = img
 
-    matrix = k_height * k_width
-    kernel = kernel.reshape(-1)
-    #calculate the output image
-    for i in xrange(i_width):
-        for j in xrange(i_height):
-            cross_image = np.reshape(cross_corr_operation[j:j+k_height, i:i+k_width], (matrix, i_rgb))
-            cross_corr_save[j, i] = np.dot(kernel, cross_image)
+#     matrix = k_height * k_width
+#     kernel = kernel.reshape(-1)
+#     #calculate the output image
+#     for i in xrange(i_width):
+#         for j in xrange(i_height):
+#             cross_image = np.reshape(cross_corr_operation[j:j+k_height, i:i+k_width], (matrix, i_rgb))
+#             cross_corr_save[j, i] = np.dot(kernel, cross_image)
 
-    return cross_corr_save
+#     return cross_corr_save
 
 def convolve_2d(img, kernel):
     '''Use cross_correlation_2d() to carry out a 2D convolution.
