925b9a09c3863d5f59727687a737c9b1d518c495
EdwardFengsc <31291202+EdwardFengsc@users.noreply.github.com>
2018-02-13 00:26:07 -0500

    Update hybrid.py

--- hybrid.py
+++ hybrid.py
@@ -25,8 +25,8 @@ def cross_correlation_2d(img, kernel):
         height and the number of color channels)
     '''
     k_height, k_width=kernel.shape
-
-    cross_corr_save=np.empty(img.shape)
+cross_corr_save
+    =np.empty(img.shape)
 
     # keep the image into 3 dimensions
     if len(img.shape)==2:
@@ -50,7 +50,7 @@ def cross_correlation_2d(img, kernel):
     #calculate the output image
     for i in xrange(i_width):
         for j in xrange(i_height):
-            cross_image = np.reshape(newpad[j:j+k_height, i:i+k_width], (matrix, i_rgb))
+            cross_image = np.reshape(cross_corr_operation[j:j+k_height, i:i+k_width], (matrix, i_rgb))
             cross_corr_save[j, i] = np.dot(kernel, cross_image)
 
     return cross_corr_save
