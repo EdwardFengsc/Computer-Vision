b65483948941b168f1dfb55d73f683fb4ccf5ec9
EdwardFengsc <31291202+EdwardFengsc@users.noreply.github.com>
2018-02-13 00:24:32 -0500

    Update hybrid.py

--- hybrid.py
+++ hybrid.py
@@ -38,7 +38,7 @@ def cross_correlation_2d(img, kernel):
         
 
     #set up a new workplace adding size of kernels and images
-    newpad = np.zeros(( k_height + k_height - 1, k_width + i_width - 1,i_rgb), dtype=img.dtype)
+    newpad = np.zeros(( k_height + i_height - 1, k_width + i_width - 1,i_rgb), dtype=img.dtype)
     
     k_height1 = (k_height - 1) / 2
     k_width1 = (k_width - 1) / 2
